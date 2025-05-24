7c4acbaf63178e9ce35721fffdc4fc6b5bcf7fda
ankitWork2001 <ankitkr.work2001@gmail.com>
2025-05-24 16:56:10 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ import { getEmployeeById, updateUser } from "../controllers/userController.js";
 const router = Router();
 
 router.get("/:employeeId", getEmployeeById);
-router.put("/", authenticate, updateUser);
+router.put("/update", authenticate, updateUser);
 router.post("/avatar", authenticate);
 
 export default router;
